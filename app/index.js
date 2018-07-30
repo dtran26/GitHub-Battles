c28e240f5214f3d1f9f70ba164aa7d6abcb6dd6a
Dan Tran <danganhtran@me.com>
2018-07-30 23:56:40 +0300

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index');
+
+class App extends React.component {
+    render() {
+        return (
+            <div>
+                Hello world!
+            </div>
+        )
+    }
+
+}
