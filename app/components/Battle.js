cdcaf13087fc89a4908e1a80aae5b5f9bcdda21a
Dan Tran <danganhtran@me.com>
2018-08-07 16:16:17 +0300

    Start implementing battle screen

---
+++ app/components/Battle.js
@@ -0,0 +1,51 @@
+var React = require("react");
+
+class Battle extends React.Component {
+  constructor(){
+    super(props);
+
+    this.state = {
+      playerOneName = '',
+      playerTwoName = '',
+      playerOneImage = null,
+      playerTwoImage = null
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(id, username){
+    this.setState(function(){
+      var newState = {};
+      newState[id + 'Name'] = username;
+      newState[id + 'Image'] = 'https://github.com/'+username+'.png?size=200';
+      return newState
+    })
+  }
+
+  render() {
+    var playerOneName = this.state.playerOneName;
+    var playerTwoName = this.state.playerTwoName;
+    return (
+    <div>
+      <div className="row">
+        {!playerOneName &&
+        <PlayerInput 
+          id='playerOne' 
+          label='PlayerOne'
+          onSubmit={this.handleSubmit}/>}
+
+        {!playerTwoName &&
+        <PlayerInput 
+          id='playerTwo'
+          label='PlayerTwo'
+          onSubmit={this.handleSubmit} />}
+      </div>
+    </div>
+    );
+  }
+}
+
+module.exports = Battle;
+
+
