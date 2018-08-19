9be57cb7dcd1928dd7b411f6140a02abcb99af1a
Dan Tran <danganhtran@me.com>
2018-08-19 23:48:16 +0300

    Refactor with ES6 syntax

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,14 +1,12 @@
-var React = require("react");
-var PropTypes = require("prop-types");
-var queryString = require("query-string");
-var api = require("../utils/api");
-var Link = require("react-router-dom").Link;
-var PlayerPreview = require("./PlayerPreview");
-var Loading = require("./Loading");
+import React from "react";
+import PropTypes from "prop-types";
+import queryString from "query-string";
+import { battle } from "../utils/api";
+import { Link } from "react-router-dom";
+import PlayerPreview from "./PlayerPreview";
+import Loading from "./Loading";
 
-function Profile(props) {
-  var info = props.info;
-
+function Profile({ info }) {
   return (
     <PlayerPreview username={info.login} avatar={info.avatar_url}>
       <ul className="space-list-items">
@@ -32,12 +30,12 @@ Profile.propTypes = {
   info: PropTypes.object.isRequired
 };
 
-function Player(props) {
+function Player({ label, score, profile }) {
   return (
     <div>
-      <h1 className="header">{props.label}</h1>
-      <h3 style={{ textAlign: "center" }}>Score: {props.score}</h3>
-      <Profile info={props.profile} />
+      <h1 className="header">{label}</h1>
+      <h3 style={{ textAlign: "center" }}>Score: {score}</h3>
+      <Profile info={profile} />
     </div>
   );
 }
@@ -49,50 +47,41 @@ Player.propTypes = {
 };
 
 class Results extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      winner: null,
-      loser: null,
-      error: null,
-      loading: true
-    };
-  }
-
-  componentDidMount() {
-    var players = queryString.parse(this.props.location.search);
-    api.battle([players.playerOneName, players.playerTwoName]).then(
-      function(results) {
-        if (results === null) {
-          return this.setState(function() {
-            return {
-              error:
-                "Looks like there was an error. Check that both users exist on Github",
-              loading: false
-            };
-          });
-        }
-        this.setState(function() {
-          return {
-            error: null,
-            winner: results[0],
-            loser: results[1],
-            loading: false
-          };
-        });
-      }.bind(this)
+  state = {
+    winner: null,
+    loser: null,
+    error: null,
+    loading: true
+  };
+  async componentDidMount() {
+    const { playerOneName, playerTwoName } = queryString.parse(
+      this.props.location.search
     );
+
+    const players = await battle([playerOneName, playerTwoName]);
+
+    if (players === null) {
+      return this.setState(() => ({
+        error:
+          "Looks like there was an error. Check that both users exist on Github.",
+        loading: false
+      }));
+    }
+
+    this.setState(() => ({
+      error: null,
+      winner: players[0],
+      loser: players[1],
+      loading: false
+    }));
   }
   render() {
-    var error = this.state.error;
-    var winner = this.state.winner;
-    var loser = this.state.loser;
-    var loading = this.state.loading;
+    const { error, winner, loser, loading } = this.state;
 
-    if (loading) {
+    if (loading === true) {
       return <Loading />;
     }
+
     if (error) {
       return (
         <div>
@@ -101,6 +90,7 @@ class Results extends React.Component {
         </div>
       );
     }
+
     return (
       <div className="row">
         <Player label="Winner" score={winner.score} profile={winner.profile} />
@@ -110,4 +100,4 @@ class Results extends React.Component {
   }
 }
 
-module.exports = Results;
+export default Results;
